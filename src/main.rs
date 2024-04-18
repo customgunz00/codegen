8753313471b86831814589e2fb903bd806321f4e
customgunz00 <159488157+customgunz00@users.noreply.github.com>
2024-04-17 23:25:35 -0600

    mainloop

--- src/main.rs
+++ src/main.rs
@@ -1,2 +1,18 @@
-use rand::distributions::{Alphanumeric, DistString};
+use rand::distributions::Alphanumeric;
+use rand::{random, Rng};
 
+fn randstring() -> String {
+  let mut rng = rand::thread_rng();
+  let random_bytes: Vec<u8> = rng.sample_iter(Alphanumeric).take(10).collect();
+  let random_string: String = random_bytes.into_iter()
+      .map(|byte| char::from(byte))
+      .collect();
+  return random_string;
+}
+
+fn main() {
+    loop {
+        let random_string: String = randstring();
+        println!("{}", random_string);
+    }
+}
